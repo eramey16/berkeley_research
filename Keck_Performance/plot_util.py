f3b51b5b5514dd8b461d3606e0663f95c7c72a71
Emily Ramey <emily_ramey@berkeley.edu>
2020-07-24 09:58:13 -0700

    added plots

--- Keck_Performance/plot_util.py
+++ Keck_Performance/plot_util.py
@@ -6,9 +6,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
+from scipy.io import readsav
+from astropy.stats import sigma_clip
+import os
 
 plot_dir = "plots/"
+tel_plots = plot_dir+"telemetry_plots/"
+weather_plots = plot_dir+"data_on_data/"
 sub_ap_file = "ao_telemetry/sub_ap_map.txt"
+
+good_color = 'blue'
+bad_colors = [(1,1,0), (1,128/255.0,0), (1,0,0)]
 
 default_settings = {
     'label': {
@@ -59,7 +67,7 @@ default_fontsize = 12
 ### Grid plotting function
 def plot_vars(data, x_vars, y_vars, c_var=None, settings=default_settings,
               figsize=default_figsize, fontsize=default_fontsize, cmap='viridis',
-              save=False, savefile=None
+              save=False, filename=None
              ):
     """
     Plots each x-variable against each y-variable in a grid
@@ -112,15 +120,15 @@ def plot_vars(data, x_vars, y_vars, c_var=None, settings=default_settings,
     plt.colorbar(color, cax = color_ax).set_label(label=settings['label'][c_var],size=fontsize)
     
     if save:
-        if savefile is None:
-            savefile = "VS"
+        if filename is None:
+            filename = "VS"
             for x in x_vars:
-                savefile = x+"_"+savefile if x not in settings['abbrv'] else settings['abbrv'][x]+"_"+savefile
+                filename = x+"_"+filename if x not in settings['abbrv'] else settings['abbrv'][x]+"_"+filename
             for y in y_vars:
-                savefile = savefile+"_"+y if y not in settings['abbrv'] else savefile+"_"+settings['abbrv'][y]
-            savefile += ".png"
+                filename = filename+"_"+y if y not in settings['abbrv'] else filename+"_"+settings['abbrv'][y]
+            filename += ".png"
         
-        plt.savefig(plot_dir+"data_on_data/"+savefile, bbox_inches='tight')
+        plt.savefig(weather_plots+filename, bbox_inches='tight')
         
     
     plt.show()
@@ -128,12 +136,14 @@ def plot_vars(data, x_vars, y_vars, c_var=None, settings=default_settings,
 
 
 
-def plot_lenslets(tel_data, lnum, shape=None, xlim={}, ylim={}, 
+def plot_lenslets(data_file, lnum, shape=None, xlim={}, ylim={}, 
                   fontsize=10, figsize=None, save=False):
     """
-    Plots centroid offset of a lenslet in the tel_data array
+    Plots centroid offset of a lenslet in the data array
     lnum: integer, subaperture index from 0 to 304
     """
+    data = readsav(data_file)
+    
     # Check on values passed
     if type(lnum) is int:
         lnum = [lnum]
@@ -155,8 +165,8 @@ def plot_lenslets(tel_data, lnum, shape=None, xlim={}, ylim={},
         ax = axes[i]
         
         # Mean of x and y offset centroids
-        x_offset = tel_data.a.offsetcentroid[0][:, num*2]
-        y_offset = tel_data.a.offsetcentroid[0][:, num*2+1]
+        x_offset = data.a.offsetcentroid[0][:, num*2]
+        y_offset = data.a.offsetcentroid[0][:, num*2+1]
         mx = x_offset.mean()
         my = y_offset.mean()
 
@@ -189,17 +199,29 @@ def plot_lenslets(tel_data, lnum, shape=None, xlim={}, ylim={},
         ax.grid()
     
     if save:
-        filename = plot_dir+"telemetry_plots/"+"_".join(map(str, lnum))+"_centroids.png"
+        filename = tel_plots+"_".join(map(str, lnum))+"_centroids.png"
         plt.savefig(filename, bbox_inches='tight')
     
     plt.show()
     return
 
-def plot_array(data, map_file=sub_ap_file, spacing=0.2, start=(0.1,0.1),
-               size=100, cmap = mpl.cm.viridis, figsize=(12, 12), fontsize=18):
+def plot_array(data_file, data_type='offset centroid', map_file=sub_ap_file, spacing=0.2,
+               start=(0.1,0.1), sig_clip=None, size=100, cmap = mpl.cm.viridis, 
+               figsize=(10, 10), fontsize=18, save=False, filename=None):
     """ Plots an array of lenslets with the standard deviation of their centroid offsets """
+    data = readsav(data_file)
+    if data_type=="offset centroid":
+        data = data.a.offsetcentroid[0]
+        clabel = "Offset Centroid $\sigma$"
+    elif data_type=="residual wavefront":
+        data = data.a.residualwavefront[0]
+        clabel = "Deformable Mirror $\sigma$ [volt]"
+    
+    if sig_clip is not None and type(sig_clip) is float:
+        sig_clip = [sig_clip]
+    
     # Array of 1s and 0s for lenslet positions in a square grid
-    sub_ap_map = pd.read_csv("ao_telemetry/sub_ap_map.txt", delim_whitespace=True, header=None).to_numpy()
+    sub_ap_map = pd.read_csv(map_file, delim_whitespace=True, header=None).to_numpy()
     len_x = sub_ap_map.shape[0]
     len_y = sub_ap_map.shape[1]
     
@@ -217,17 +239,66 @@ def plot_array(data, map_file=sub_ap_file, spacing=0.2, start=(0.1,0.1),
     xx = xx[xx!=0]
     yy = yy[yy!=0]
     
-    # Find standard deviation of offset centroids
-    x_std = np.std(data.a.offsetcentroid[0][:, range(0, len(xx)*2, 2)], axis=0)
-    y_std = np.std(data.a.offsetcentroid[0][:, range(1, len(yy)*2, 2)], axis=0)
-    std_all = np.sqrt(x_std**2 + y_std**2)
+    if data_type=='offset centroid':
+        # Find standard deviation of offset centroids
+        xrange = range(0, len(xx)*2, 2)
+        yrange = range(1, len(yy)*2, 2)
+        x_std = np.std(data[:, xrange], axis=0)
+        y_std = np.std(data[:, yrange], axis=0)
+        std_all = np.sqrt(x_std**2 + y_std**2)
+    elif data_type=='residual wavefront':
+        # Find standard deviation of residual wavefront
+        std_all = np.std(data, axis=0)
+        std_all = std_all[range(len(xx))]
     
     # Plot resulting lenslet array
     fig = plt.figure(figsize = figsize)
-    color = plt.scatter(xx, yy, s = std_all * size, c = std_all, cmap = cmap)
-    for i in range(len(x_std)):
-        plt.text(xx[i], yy[i] - 0.1, i, horizontalalignment='center')
-    # Add colorbar
-    color_ax = fig.add_axes([.91, .125, 0.02, 0.755])
-    plt.colorbar(color, cax = color_ax).set_label(label='Offset Centroid $\sigma$',
+    
+    # Set up different graphs
+    if sig_clip is None:
+        color = plt.scatter(xx, yy, s = std_all * size, c = std_all, cmap = cmap)
+    else:
+        sig_clip = np.sort(sig_clip)
+        # Plot
+        plt.scatter(xx, yy, s=std_all*size, c=good_color, label=f"points < {sig_clip[0]}$\sigma$")
+        
+        start = len(bad_colors)-len(sig_clip)
+        for i,sig in enumerate(sig_clip):
+            # Mask as integer array
+            q = sigma_clip(std_all, sigma=sig).mask.astype(int)
+            # Colors
+            rgba_colors = np.zeros((len(std_all),4))
+            rgba_colors[:,0:3] = bad_colors[start+i]
+            rgba_colors = rgba_colors
+            rgba_colors[:,3] = q
+            # Plot
+            label = f"points>{sig}$\sigma$" if i==len(sig_clip)-1 else f"points<{sig_clip[i+1]}$\sigma$"
+            plt.scatter(xx, yy, s=std_all*size, c=rgba_colors, label=label)
+        # Legend
+        leg = plt.legend()
+        [leg.legendHandles[i+1].set_color(bad_colors[start+i]) for i in range(len(bad_colors)-start)]
+    
+    # Add text
+    textoffset = 0.1 if data_type=="offset centroid" else 4
+    for i in range(len(xx)):
+        plt.text(xx[i], yy[i] - textoffset, i, horizontalalignment='center')
+    plt.xlabel("X Coordinate", fontsize=fontsize)
+    plt.ylabel("Y Coordinate", fontsize=fontsize)
+    
+    if sig_clip is None:
+        # Add colorbar
+        color_ax = fig.add_axes([.91, .125, 0.02, 0.755])
+        plt.colorbar(color, cax = color_ax).set_label(label=clabel,
                                                   fontsize=fontsize)
+    if save:
+        if not filename:
+            filename = data_file.split("/")
+            filename = filename[5]+"_"+filename[-1].split(".")[0].split("_")[0]
+            filename += "_"+data_type.split(" ")[0]+"_std"
+            if sig_clip is not None:
+                filename+="_sig"
+                for sig in sig_clip:
+                    filename+="_"+str(np.round(float(sig), 2)).replace(".", "_")
+            filename+=".png"
+        filename = tel_plots+filename
+        plt.savefig(filename, bbox_inches='tight')
